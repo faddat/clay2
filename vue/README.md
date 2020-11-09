aa10a9cfd82db88bece7ecd519a85116558b847a
Developer Experience team at Tendermint <hello@tendermint.com>
2020-11-09 22:49:13 +0700

    Initialized with Starport

---
+++ vue/README.md
@@ -0,0 +1,27 @@
+# clay2 UI
+
+This is the [vuejs](https://vuejs.org/) user inteface to clay2.
+
+It provides UI components including login, and a template for you to do additonal development.
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ x/clay2/keeper/params.go
@@ -0,0 +1,22 @@
+package keeper
+/*
+// TODO: Define if your module needs Parameters, if not this can be deleted
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/faddat/clay2/x/clay2/types"
+)
+
+// GetParams returns the total set of clay2 parameters.
+func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+	k.paramspace.GetParamSet(ctx, &params)
+	return params
+}
+
+// SetParams sets the clay2 parameters to the param space.
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.paramspace.SetParamSet(ctx, &params)
+}
+*/
